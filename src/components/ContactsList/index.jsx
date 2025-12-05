c9d118d97edf08857e0d545f2fd99b05794565d9
GromavVL <vlad086367@gmail.com>
2025-12-05 12:13:29 +0200

    deleteContact and updateContact

--- src/components/ContactsList/index.jsx
+++ src/components/ContactsList/index.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { deleteContact, updateContact } from '../../store/slices/contactSlice'
 
-function ContactsList ({ contacts }) {
+function ContactsList ({ contacts, deleteContactById, updateContactById }) {
+  // const isFavouriteChanged = () => {}
+  const changeIsFavourite = (id, checked) =>
+    updateContactById(id, { isFavourite: checked })
   return (
     <ul>
       {contacts.map(c => (
-        <li key={c.id}>{JSON.stringify(c)}</li>
+        <li key={c.id}>
+          <label>
+            <input
+              type='checkbox'
+              checked={c.isFavavorite}
+              onChange={({ target: { checked } }) =>
+                changeIsFavourite(c.id, checked)
+              }
+            />{' '}
+          </label>
+          {JSON.stringify(c)}
+          <button onClick={() => deleteContactById(c.id)}>Delete</button>
+        </li>
       ))}
     </ul>
   )
 }
 
 const mapStateToProps = ({ contactsList }) => contactsList
-export default connect(mapStateToProps)(ContactsList)
+
+const mapDispatchToProps = dispach => ({
+  deleteContactById: id => dispach(deleteContact(id)),
+  updateContactById: (id, data) => dispach(updateContact({ id, data }))
+})
+export default connect(mapStateToProps, mapDispatchToProps)(ContactsList)
--- src/store/slices/contactSlice.js
+++ src/store/slices/contactSlice.js
@@ -5,11 +5,13 @@ const initialState = {
   contacts: [
     {
       id: uuidv4(),
+      isFavavorite: false,
       fullName: "John Doe",
       phoneNumber: "+1234567890",
     },
     {
       id: uuidv4(),
+      isFavavorite: false,
       fullName: "Jane Smith",
       phoneNumber: "+0987654321",
     },
@@ -19,8 +21,20 @@ const initialState = {
 const contactSlice = createSlice({
   initialState,
   name: "contacts",
-  reducers: {},
+  reducers: {
+    deleteContact: (state, { payload }) => {
+      state.contacts = state.contacts.filter((c) => c.id !== payload);
+    },
+    updateContact: (state, { payload: { id, data } }) => {
+      const contactIndex = state.contacts.findIndex((c) => c.id === id);
+      state.contacts[contactIndex] = {
+        ...state.contacts[contactIndex],
+        ...data,
+      };
+    },
+  },
 });
 
 export const { reducer, actions } = contactSlice;
+export const { deleteContact, updateContact } = actions;
 export default reducer;
